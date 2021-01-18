a2a99ab674669ae8ef12f196b2412d0fff0ada86
Thiago Sales <thiagomsales@hotmail.com>
2021-01-17 23:11:19 -0300

    :construction::construction_worker: FileGrid/File

--- components/FileGrid/index.tsx
+++ components/FileGrid/index.tsx
@@ -1,25 +1,28 @@
 
 import styles from './style.module.css';
+import { AiFillFile } from 'react-icons/Ai';
 
 export default function FileGrid() {
     return (
         <div className={styles.fileGrid}>
             <div className={styles.fileItem}>
-                <div>
-
+                <div className={styles.filePreview}>
+                    <AiFillFile />
                 </div>
-                <div>
-
+                <div className={styles.fileInfo}>
+                    <div className={styles.fileName}>
+                        <span>
+                            File.txt
+                    </span>
+                    </div>
+                    <div className={styles.fileDateModify}>
+                        <span>
+                            Modificado: 20/05/2020 08:00:00
+                    </span>
+                    </div>
                 </div>
             </div>
-            <div className={styles.fileItem}>
-                <div>
 
-                </div>
-                <div>
-
-                </div>
-            </div>
         </div>
     )
 }
