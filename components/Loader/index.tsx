803cbaab8d5bb49a8db276c9841519db27a86d7d
Thiago Sales <thiagomsales@hotmail.com>
2021-01-23 12:41:35 -0300

    Correção do Loader

---
+++ components/Loader/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './style.module.css';
+
+export default function index() {
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.ldsgrid}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        </div>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,12 @@ import ActionBar from '../components/ActionBar';
 import FileGrid from '../components/FileGrid';
 import HomeLayout from '../layouts/Home';
 import { FileContextProvider } from '../contexts/File';
-
+import Loader from '../components/Loader';
 export default function Home() {
   return (
     <FileContextProvider>
       <HomeLayout>
+      <Loader/>
         <ActionBar />
         <FileGrid />
       </HomeLayout>
