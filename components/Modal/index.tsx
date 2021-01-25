66ef904eb9f5d6f99a0b3f1294b396a041cee158
Thiago Sales <thiagomsales@hotmail.com>
2021-01-24 22:41:54 -0300

    hotfix import

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AiOutlineClose } from "react-icons/Ai";
+import { AiOutlineClose } from "react-icons/ai";
 import styles from "./style.module.css";
 
 interface Props {
